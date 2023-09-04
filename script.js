e1496be6f8b3680067d1cd3041df2fb053c24097
Matiascyc <snake_cuadro@hotmail.com>
2023-09-04 10:45:19 -0300

    Paginainteraccion

---
+++ script.js
@@ -0,0 +1,21 @@
+let nombre = prompt("Por favor, ingrese el nombre de tu heroe")
+
+if (nombre != null) {
+    document.write(nombre);
+}
+else {
+    alert("Por favor ingrese algun nombre o edad, Recargue la pagina para continuar");
+}
+
+/* no se donde puede estar el error, la idea era armar un alert que te diga como se llamaba tu heroe
+con la edad al momento de morir- Despues hacer un array para mostrar en el index cuantas personas lo intentaron
+y como se llamaba cada heroe que lo intento
+
+const muerte2 = document.getElementById("muerte");
+muerte2.onclick = clickboton
+function clickboton() {
+    alert("tu heroe murio con" + edad + ".Intentalo denuevo");
+
+}
+*/
+
